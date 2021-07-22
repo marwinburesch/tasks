1eb73690a94f937f3bed1daf9ade3ad319e023b3
marwinburesch <marwin@posteo.de>
2021-07-22 14:04:11 +0200

    Add story for actionButton

---
+++ src/components/actionButton/actionButton.stories.js
@@ -0,0 +1,9 @@
+import "./actionButton.css";
+import actionButton from "./actionButton.html";
+
+export default {
+  title: "Components/ActionButton",
+  parameters: { layout: "fullscreen" },
+};
+
+export const addTask = () => actionButton;
