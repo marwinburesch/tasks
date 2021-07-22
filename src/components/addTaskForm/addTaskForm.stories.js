2ba7fa330ed030925d8c251f5752e744d898dd1d
marwinburesch <marwin@posteo.de>
2021-07-22 15:55:38 +0200

    Add story for addTaskForm

---
+++ src/components/addTaskForm/addTaskForm.stories.js
@@ -0,0 +1,9 @@
+import "./addTaskForm.css";
+import addTaskForm from "./addTaskForm.html";
+
+export default {
+  title: "Components/Forms",
+  parameters: { layout: "centered" },
+};
+
+export const addTask = () => addTaskForm;
