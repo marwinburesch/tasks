deefe329520775c584e280c86dadeeffe09d4616
marwinburesch <marwin@posteo.de>
2021-07-26 12:04:31 +0200

    Add taskItem story

---
+++ src/components/taskItem/taskItem.stories.js
@@ -0,0 +1,9 @@
+import "./taskItem.css";
+import taskItemRegular from "./taskItemRegular.html";
+
+export default {
+  title: "Components/TaskItem",
+  parameters: { layout: "centered" },
+};
+
+export const regular = () => taskItemRegular;
