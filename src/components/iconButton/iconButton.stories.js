e1b0671b4d57d3f1a995c0943203bd0a6a242640
marwinburesch <marwin@posteo.de>
2021-07-26 15:37:53 +0200

    Add iconButton story

---
+++ src/components/iconButton/iconButton.stories.js
@@ -0,0 +1,9 @@
+import "./iconButton.css";
+import iconButton from "./iconButton.html";
+
+export default {
+  title: "Components/IconButton",
+  parameters: { layout: "centered" },
+};
+
+export const regular = () => iconButton;
