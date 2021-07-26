19a2c8bd155f5c6878a91ec24b1b51fd08b1152f
marwinburesch <marwin@posteo.de>
2021-07-26 15:05:48 +0200

    Add taskNav story

---
+++ src/components/taskNav/taskNav.stories.js
@@ -0,0 +1,9 @@
+import "./taskNav.css";
+import taskNav from "./taskNav.html";
+
+export default {
+  title: "Components/TaskNav",
+  parameters: { layout: "centered" },
+};
+
+export const regular = () => taskNav;
