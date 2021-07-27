2a866d4f4cbb723ad413dea0b70f2b3bde4b2109
phlgr <philipp@gartz.dev>
2021-07-27 16:47:01 +0200

    Add createTaskListItem function

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ function sendAlert() {
 
 const taskList = document.querySelector(".taskList");
 
-const newTask = document.createElement("p");
-newTask.innerText = "Example Task";
+const taskOne = createTaskListItem("Kaffee kochen");
+const taskTwo = createTaskListItem("Javascript lernen");
 
-taskList.append(newTask);
+taskList.append(taskOne, taskTwo);
 
-function createTaskListItem() {
+function createTaskListItem(taskName) {
   const taskListItem = document.createElement("label");
+  const input = document.createElement("input");
+  const span = document.createElement("span");
+
   taskListItem.className = "taskItem";
-  // write code here
+
+  input.className = "taskItem__checkbox";
+  input.type = "checkbox";
+  input.setAttribute("name", "tasks");
+
+  span.className = "taskItem__labelText";
+  span.innerText = taskName;
+
+  taskListItem.append(input, span);
 
   return taskListItem;
 }
