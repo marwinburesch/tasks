cabc89d956a04eba5212c3346c4c5a848ad5ea22
phlgr <philipp@gartz.dev>
2021-07-27 15:10:05 +0200

    Prepare createTaskListItem function

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const newTask = document.createElement("p");
 newTask.innerText = "Example Task";
 
 taskList.append(newTask);
+
+function createTaskListItem() {
+  const taskListItem = document.createElement("label");
+  taskListItem.className = "taskItem";
+  // write code here
+
+  return taskListItem;
+}
