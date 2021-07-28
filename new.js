c0e5adc44addd3cf91db8acc6d2ab0a8d14b7bd3
marwinburesch <marwin@posteo.de>
2021-07-28 13:59:06 +0200

    Add two approaches to getting data from form

---
+++ new.js
@@ -0,0 +1,27 @@
+// Display sleceted date onclick, button not part of form
+const altButton = document.querySelector(".addTask__alternativeButton");
+
+altButton.onclick = alertSelectedDate;
+
+function alertSelectedDate() {
+  const checkedDateInput = document.querySelector(
+    ".addTaskForm__radio:checked"
+  );
+  alert(checkedDateInput.value);
+}
+
+// Display sleceted date onclick, using submit button
+const form = document.querySelector(".addTaskForm");
+
+form.onsubmit = function (event) {
+  // Prevent the default functionality of the submit event, which is reloading the page
+  event.preventDefault();
+  const checkedDateInput = document.querySelector(
+    ".addTaskForm__radio:checked"
+  );
+  const textInput = document.querySelector(".addTaskForm__textInput");
+
+  const selectedDate = checkedDateInput.value;
+  const taskName = textInput.value;
+  console.log(selectedDate, taskName);
+};
