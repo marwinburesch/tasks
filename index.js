37d1ddec1cf59d55069a77579c268205b4df0bfa
marwinburesch <marwin@posteo.de>
2021-07-28 16:30:54 +0200

    Modify createTaskListItem to accept objects as argument

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ function sendAlert() {
 }
 
 const taskList = document.querySelector(".taskList");
+// Placeholder
+const potentialTask = {
+  title: "Get groceries",
+  date: "Tomorrow",
+  isDone: true,
+};
 
-const taskOne = createTaskListItem("Kaffee kochen");
-const taskTwo = createTaskListItem("Javascript lernen");
+const taskFromObject = createTaskListItem(potentialTask);
 
-taskList.append(taskOne, taskTwo);
+taskList.append(taskFromObject);
 
-function createTaskListItem(taskName) {
+function createTaskListItem(task) {
   const taskListItem = document.createElement("label");
   const input = document.createElement("input");
   const span = document.createElement("span");
@@ -22,9 +27,10 @@ function createTaskListItem(taskName) {
   input.className = "taskItem__checkbox";
   input.type = "checkbox";
   input.setAttribute("name", "tasks");
+  input.checked = task.isDone;
 
   span.className = "taskItem__labelText";
-  span.innerText = taskName;
+  span.innerText = task.title;
 
   taskListItem.append(input, span);
 
