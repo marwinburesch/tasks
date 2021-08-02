7aa1865abadfd97dfe899a3b749ca461cfa30c53
phlgr <philipp@gartz.dev>
2021-08-02 11:33:00 +0200

    Add functionality to complete Tasks on click

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { parseJSONFromLocalStorage } from "./utils/localstorage.js";
+import {
+  parseJSONFromLocalStorage,
+  stringifyJSONToLocalStorage,
+} from "./utils/localstorage.js";
 
 const taskList = document.querySelector(".taskList");
 
@@ -7,6 +10,13 @@ const tasks = parseJSONFromLocalStorage("tasks", []);
 const taskListItems = tasks.map((task) => createTaskListItem(task));
 
 taskList.append(...taskListItems);
+
+function completeTask(taskName, completed) {
+  const tasks = parseJSONFromLocalStorage("tasks", []);
+  const task = tasks.find((task) => task.title === taskName);
+  task.isDone = completed;
+  stringifyJSONToLocalStorage("tasks", tasks);
+}
 
 function createTaskListItem(task) {
   const taskListItem = document.createElement("label");
@@ -19,6 +29,7 @@ function createTaskListItem(task) {
   input.type = "checkbox";
   input.setAttribute("name", "tasks");
   input.checked = task.isDone;
+  input.onclick = () => completeTask(task.title, input.checked);
 
   span.className = "taskItem__labelText";
   span.innerText = task.title;
